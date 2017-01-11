07ee8d1ee659bb848e66e5613befb75e9a7dabf5
ShurikuS <cawa007@gmail.com>
2017-01-11 10:36:59 +0300

    Add unit test from SearchNamePresenter.

---
+++ app/src/test/java/com/taptm/shurikus/githubviewer/searchname/SearchNamePresenterTest.java
@@ -0,0 +1,70 @@
+package com.taptm.shurikus.githubviewer.searchname;
+
+
+import com.taptm.shurikus.githubviewer.R;
+import com.taptm.shurikus.githubviewer.data.FakeRemoteDataSource;
+import com.taptm.shurikus.githubviewer.data.User;
+import com.taptm.shurikus.githubviewer.data.source.DataSource;
+import com.taptm.shurikus.githubviewer.data.source.Repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SearchNamePresenterTest {
+
+    private List<User> users;
+
+    @Mock
+    private Repository mRepository;
+
+    @Mock
+    private SearchNameContract.View mView;
+
+    @Captor
+    private ArgumentCaptor<DataSource.LoadUsersCallback> mLoadUsersCallbackCaptor;
+
+    private SearchNamePresenter mPresenter;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mPresenter = new SearchNamePresenter(mRepository, mView);
+
+        when(mView.isActive()).thenReturn(true);
+
+        users = FakeRemoteDataSource.getFakeUsers();
+
+    }
+
+    @Test
+    public void searchEmptyString_CallViewToDisplay() throws Exception {
+        users.clear();
+
+        mPresenter.searchUser("");
+        verify(mView).showMessage(R.string.msg_search_not_be_empty);
+    }
+
+    @Test
+    public void searchNoEmptyStringFromRepository_CallViewToDisplay(){
+        mPresenter.searchUser("s");
+
+        verify(mRepository).searchUsers(eq("s"), mLoadUsersCallbackCaptor.capture());
+        mLoadUsersCallbackCaptor.getValue().onUsersLoaded(users);
+
+        verify(mView).showUsers(users);
+    }
+
+}
